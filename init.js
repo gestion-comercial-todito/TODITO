4bb9d7456fe064c717a9b229f1c443d497c60f53
gestion-comercial-todito <contacto.toditoctes@gmail.com>
2025-10-06 15:40:11 -0300

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAo5MFidQ23UwQqTPhxHVzRKaZVoxmvj0",
+  authDomain: "gestion-toditoctes.firebaseapp.com",
+  projectId: "gestion-toditoctes",
+  storageBucket: "gestion-toditoctes.firebasestorage.app",
+  messagingSenderId: "549059339665",
+  appId: "1:549059339665:web:3387d4f7eeed5470e66630"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
